2ac67b744a040c159a6e76b8448471329e95b795
Rushikesh Patil <166911774+rushikeshpatil9891@users.noreply.github.com>
2026-06-23 12:15:38 +0530

    Update app.js
    This is the final commit.

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
 main()
     .then( ()=>{
-        console.log("Connected to DB");
+        console.log("Connect to DB");
     })
     .catch((err)=>{
         console.log(err);
